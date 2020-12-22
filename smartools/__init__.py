a380af663c1dd99160b79587e950951396424951
David Carli-Arnold <davocarli@gmail.com>
2020-12-21 19:21:26 -0500

    Updated __init__ to define additional variables

--- smartools/__init__.py
+++ smartools/__init__.py
@@ -1,1 +1,8 @@
-from smartools import smartools
+from smartsheet import * # Import all the modules from smartsheet.
+import smartsheet # Import smartsheet directly so that it will run it's __init__
+
+for var in dir(smartsheet): # Define all variables locally that are defined during 
+	if var not in dir():	# smartsheet's __init__ such as __gov_base__ or __api_base__.
+		exec(var + ' = smartsheet.' + var)
+
+from smartools.smartools import Smartsheet # Import our new custom subclass
