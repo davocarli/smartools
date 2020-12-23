0cfc0007d9772ef0cd1778d49bb70e28d79c1316
davocarli <davocarli@gmail.com>
2020-12-23 14:06:42 -0800

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'smartools',
   packages = ['smartools'],
-  version = '0.2',
+  version = '0.3',
   license='MIT',
   description = 'A class extending the official smartsheet-python-sdk that adds a new Util subclass with useful helper methods.',
   author = 'David Carli-Arnold',
