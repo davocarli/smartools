f7db79fe043c22ccbae2039daad1d7c6d8abcb52
davocarli <davocarli@gmail.com>
2020-12-31 10:36:07 -0500

    Added argument comments & updated some variable names

--- smartools/smartools.py
+++ smartools/smartools.py
@@ -24,28 +24,40 @@ class Smartsheet(smartsheet.Smartsheet):
 			self.include_all = 'attachments,cellLinks,data,discussions,filters,forms,rules,ruleRecipients,all,columnType,format,objectValue,rowPermalink,rowWriterInfo,writerInfo'
 
 
-		def list_sheets_in_workspace(self, parentid, contains=None, exact=False, parent=None):
+		# Returns a list of all the sheets in a workspace. Optionally only includes sheets with specific Strings contained in the sheet name
+		def list_sheets_in_workspace(self,
+				workspace_id,  # The ID of the workspace whose sheets should be returned
+				contains=None,  # (Optional) A String that should be contained in the sheet's sheet names to be included in the return
+				exact=False,  # If set to True, will only return sheets with exactly the the name provided in the "contains" argument, rather than a partial match
+				parent=None  # Used for recursively providing information to this method. Should not be provided by users
+			):
 			sheets = []
 			if parent == None:
-				parent = self.smart.Workspaces.get_workspace(parentid, load_all=True)
+				parent = self.smart.Workspaces.get_workspace(workspace_id, load_all=True)
 			for sheet in parent.sheets:
 				if (contains is None) \
 				or (exact and contains == sheet.name) \
 				or contains in sheet.name:
 					sheets.append(sheet)
 			for folder in parent.folders:
-				sheets.extend(self.list_sheets_in_workspace(parentid=None, contains=contains, exact=exact, parent=folder))
+				sheets.extend(self.list_sheets_in_workspace(workspace_id=None, contains=contains, exact=exact, parent=folder))
 			return sheets
 
 
-		def list_containers_in_workspace(self, parentid, contains=None, exact=False, parent=None):
+		# Returns an object with lists of all containers in a workspace. Optionally only includes containers with specific Strings contained in the sheet name
+		def list_containers_in_workspace(self,
+				workspace_id,  # The ID of the workspace whose containers should be returned
+				contains=None,  # (Optional) A String that should be contained in the container's name to be included in the return
+				exact=False,  # If set to True, will only return sheets with exactly the name provided in the "contains" argument, rather than a partial match
+				parent=None  # Used for recursively providing information to this method. Should not be provided by users
+			):
 			containers={
 				'sheets': [],
 				'sights': [],
 				'reports': []
 			}
 			if parent == None:
-				parent = self.smart.Workspaces.get_workspace(parentid, load_all=True)
+				parent = self.smart.Workspaces.get_workspace(workspace_id, load_all=True)
 			for sheet in parent.sheets:
 				if (contains is None) \
 				or (exact and contains == sheet.name) \
@@ -62,33 +74,43 @@ class Smartsheet(smartsheet.Smartsheet):
 				or contains in report.name:
 					containers['reports'].append(report.name)
 			for folder in parent.folders:
-				child = self.list_containers_in_workspace(parentid=None, contains=contains, exact=exact, parent=folder)
+				child = self.list_containers_in_workspace(workspace_id=None, contains=contains, exact=exact, parent=folder)
 				containers['sheets'].extend(child.sheets)
 				containers['sights'].extend(child.sights)
 				containers['reports'].extend(child.reports)
 			return SmartoolsObject(containers)
 
 
-		def list_sheets_in_folder(self, parentid, contains=None, exact=False):
+		# Returns a list of all the sheets in a folder. Optionally only includes sheets with specific Strings contained in the sheet name
+		def list_sheets_in_folder(self,
+				folder_id,  # The ID of the folder whose sheets should be returned
+				contains=None,  # (Optional) A String that should be contained in the sheet's name to be included in the returned list
+				exact=False  # If set to True, will only return sheets with exactly the name provided in the "contains" argument, rather than a partial match
+			):
 			sheets = []
-			parent = self.smart.Folders.get_folder(parentid)
+			parent = self.smart.Folders.get_folder(folder_id)
 			for sheet in parent.sheets:
 				if (contains is None) \
 				or (exact and contains == sheet.name) \
 				or contains in sheet.name:
 					sheets.append(sheet)
 			for folder in parent.folders:
-				sheets.extend(self.list_sheets_in_folder(parentid=folder.id, contains=contains, exact=exact))
+				sheets.extend(self.list_sheets_in_folder(folder_id=folder.id, contains=contains, exact=exact))
 			return sheets
 
 
-		def list_containers_in_folder(self, parentid, contains=None, exact=False):
+		# Returns an object with lists of all containers in a workspace. Optionally only includes containers with specific Strings contained in the sheet name
+		def list_containers_in_folder(self,
+				folder_id,  # The ID of the folder whose containers should be returned
+				contains=None,  # (Optional) A String that should be contained in the container's name to be included in the returned object
+				exact=False  # If set to True, will only return containers with exactly the name provided in the "contains" argument, rather than a partial match
+			):
 			containers = {
 				'sheets': [],
 				'sights': [],
 				'reports': []
 			}
-			parent = self.smart.Folders.get_folder(parentid)
+			parent = self.smart.Folders.get_folder(folder_id)
 			for sheet in parent.sheets:
 				if (contains is None) \
 				or (exact and contains == sheet.name) \
@@ -105,19 +127,26 @@ class Smartsheet(smartsheet.Smartsheet):
 				or contains in report.name:
 					containers['reports'].append(report)
 			for folder in parent.folders:
-				child = self.list_containers_in_folder(parentid=folder.id, contains=contains, exact=exact)
+				child = self.list_containers_in_folder(folder_id=folder.id, contains=contains, exact=exact)
 				containers['sheets'].extend(child.sheets)
 				containers['sights'].extend(child.sights)
 				containers['reports'].extend(child.reports)
 			return SmartoolsObject(containers)
 
 
-		def bulk_add_rows(self, sheetid, rows, n=500, retries=5):
+		# Adds rows to a sheet. Allows you to pass a list of more than 500 rows, and automatically handles timeout errors using exponentially smaller requests
+		def bulk_add_rows(self,
+				sheet_id,  # The ID of the sheet the rows should be added to
+				rows,  # The list of rows that should be added to the sheet
+				n=500,  # The number of rows per request to begin with. Will usually be 500, but if working with a large sheet where timeouts are expected you can start smaller
+				retries=5  # The number of consecutive errors adding rows before the operation is cancelled
+			):
 			result = {
 				'responses': [],
 				'rows': [],
 				'data': [],
-				'status': ''
+				'status': '',
+				'error_message': None
 			}
 
 			current_retries = retries
@@ -129,7 +158,7 @@ class Smartsheet(smartsheet.Smartsheet):
 				rows = [rows]
 
 			while len(rows) > 0:
-				response = self.smart.Sheets.add_rows(sheetid, rows[:n])
+				response = self.smart.Sheets.add_rows(sheet_id, rows[:n])
 				if hasattr(response.result, 'error_code'):
 					current_retries -= 1
 					if response.result.error_code == 4002:
@@ -152,12 +181,19 @@ class Smartsheet(smartsheet.Smartsheet):
 			return SmartoolsObject(result)
 
 
-		def bulk_update_rows(self, sheetid, rows, n=500, retries=5):
+		# Updates rows on a sheet. Allows you to pass a list of more than 500 rows, and automatically handles timeout errors using exponentially smaller requests 
+		def bulk_update_rows(self,
+				sheet_id,  # The ID of the sheet whose rows should be updated
+				rows,  # The list of rows that should be updated
+				n=500,  # The number of rows per request to begin with. Will usually be 500, but if working with a large sheet where timeouts are expected you can start smaller
+				retries=5  # The number of consecutive errors adding rows before the operation is cancelled
+			):
 			result = {
 				'responses': [],
 				'rows': [],
 				'data': [],
-				'status': ''
+				'status': '',
+				'error_message': None
 			}
 
 			current_retries = retries
@@ -169,7 +205,7 @@ class Smartsheet(smartsheet.Smartsheet):
 				rows = [rows]
 
 			while len(rows) > 0:
-				response = self.smart.Sheets.update_rows(sheetid, rows[:n])
+				response = self.smart.Sheets.update_rows(sheet_id, rows[:n])
 				if hasattr(response.result, 'error_code'):
 					current_retries -= 1
 					if response.result.error_code == 4002:
@@ -192,13 +228,17 @@ class Smartsheet(smartsheet.Smartsheet):
 				return SmartoolsObject(result)
 
 
-		def get_sheet_as_pandas_dataframe(self, sheetid, label_column=None):
+		# Retrieves a sheet then returns a pandas DataFrame of the sheet's data
+		def get_sheet_as_pandas_dataframe(self,
+				sheet_id,  # The ID of the sheet to be returned
+				label_column=None  # The column to be used for row labels of the DataFrame. If not provided the primary column will be used
+			):
 			try:
 				import pandas as pd
 			except ImportError:
 				raise RequirementError({'message': 'Import Error: This method requires the pandas module', 'recommended_action': 'Install pandas by using "pip install pandas"'})
-			if isinstance(sheetid, int):
-				sheet = self.smart.Sheets.get_sheet(sheetid)
+			if isinstance(sheet_id, int):
+				sheet = self.smart.Sheets.get_sheet(sheet_id)
 
 				pd_row_data = []
 				pd_row_labels = []
@@ -224,14 +264,18 @@ class Smartsheet(smartsheet.Smartsheet):
 				return pd.DataFrame(pd_row_data, columns=pd_columns, index=pd_row_labels)
 
 
-		def check_sheet_permissions(self, sheetid, permission_level=None):
+		# Takes a sheet ID and minimum permission level as arguments, then returns an object including a confirmation of whether the permission level is met
+		def check_sheet_permissions(self,
+				sheet_id,  # The ID of the sheet to check for permission requirements
+				permission_level=None  # The minimum permission level required. Can be a number from 1-5, or a String. If None, method will just return the sheet permission level
+			):
 			access_levels = self.access_levels
 			try:
-				sheetid = int(sheetid)
+				sheet_id = int(sheet_id)
 			except:
 				return SmartoolsObject({'status': 'ERROR', 'access_met': False, 'Reason': 'Sheet ID is invalid'})
 
-			sheet = self.smart.Sheets.get_sheet(sheetid, column_ids=[0], row_numbers=[0], level=1)
+			sheet = self.smart.Sheets.get_sheet(sheet_id, column_ids=[0], row_numbers=[0], level=1)
 
 			if hasattr(sheet, 'result') and hasattr(sheet.result, 'error_code'):
 				return SmartoolsObject({'status': 'ERROR', 'access_met': False, 'sheet': sheet})
@@ -246,14 +290,18 @@ class Smartsheet(smartsheet.Smartsheet):
 				return SmartoolsObject({'status': 'SUCCESS', 'access_met': permission_met, 'access_level': sheet.access_level, 'sheet_response': sheet})
 
 
-		def check_workspace_permissions(self, spaceid, permission_level=None):
+		# Takes a workspace ID and minimum permission level as arguments, then returns an object including a confirmation of whether the permission level is met
+		def check_workspace_permissions(self,
+				workspace_id,  # The ID of the workspace to check for permission requirements
+				permission_level=None  # The minimum permission level required. Can be a number from 1-5, or a String. If None, method will just return the workspace permisson level
+			):
 			access_levels = self.access_levels
 			try:
-				spaceid = int(spaceid)
+				workspace_id = int(workspace_id)
 			except:
 				return SmartoolsObject({'status': 'ERROR', 'access_met': False, 'Reason': 'Workspace ID is invalid'})
 
-			space = self.smart.Workspaces.get_workspace(spaceid)
+			space = self.smart.Workspaces.get_workspace(workspace_id)
 
 			if hasattr(space, 'result') and hasattr(space.result, 'error_code'):
 				return SmartoolsObject({'status': 'ERROR', 'access_met': False, 'Reason': 'This workspace could not be found', 'workspace_response': space})
@@ -268,8 +316,11 @@ class Smartsheet(smartsheet.Smartsheet):
 				return SmartoolsObject({'status': 'SUCCESS', 'access_met': permission_met, 'access_level': space.access_level, 'workspace_response': space})
 
 
-		def check_folder_access(self, folderid):
-			folder = self.smart.Folders.get_folder(folderid)
+		# Takes a folder ID as an argument, and returns a boolean indicating whether it is a valid folder ID that the user has access to
+		def check_folder_access(self,
+				folder_id  # The ID of the folder to check for access.
+			):
+			folder = self.smart.Folders.get_folder(folder_id)
 			if hasattr(folder, 'result') and hasattr(folder.result, 'error_code'):
 				return False
 			return True
