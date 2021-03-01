3728382580693078c8b79246d4cdfd76d4ae81f2
davocarli <davocarli@gmail.com>
2021-03-01 11:29:58 -0800

    Enable Home patch

--- smartools/patches/__init__.py
+++ smartools/patches/__init__.py
@@ -2,4 +2,4 @@ from .typed_list import SmartoolsTypedList
 from .sheets import SmartoolsSheets
 from .workspaces import SmartoolsWorkspaces
 from .folders import SmartoolsFolders
-# from .home import SmartoolsHome  # Extra work required first
+from .home import SmartoolsHome
