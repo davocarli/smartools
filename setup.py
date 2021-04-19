9897616752937e79e509590511ccdde0eee5e495
davocarli <davocarli@gmail.com>
2021-04-19 11:53:04 -0700

    Update version #

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'smartools',
   packages = ['smartools', 'smartools.patches'],
-  version = '0.7.0',
+  version = '0.7.1',
   license='MIT',
   description = 'A wrapper for the smartsheet-python-sdk that monkey-patches in new methods & functionality.',
   author = 'David Carli-Arnold',
