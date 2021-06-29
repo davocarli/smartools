cdc64ca177be78090ceda8cdb5f3139737a79731
David Carli-Arnold <davocarli@gmail.com>
2021-06-29 12:37:13 -0700

    Temporary change

--- smartools/patches/__init__.py
+++ smartools/patches/__init__.py
@@ -4,4 +4,4 @@ from .workspaces import SmartoolsWorkspaces
 from .folders import SmartoolsFolders
 from .home import SmartoolsHome
 from .reports import SmartoolsReports
-from .sheet import SmartoolsSheet
+from .sheet import Sheet
