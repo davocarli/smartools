05ab038d91741521ab2bd9cf63951d435ac337c4
David Carli-Arnold <davocarli@gmail.com>
2024-11-07 14:00:10 -0300

    Add impersonation & callback functionality

--- smartools/smartools.py
+++ smartools/smartools.py
@@ -5,10 +5,42 @@ import re
 import smartsheet
 from smartsheet.smartsheet import OperationErrorResult
 
+
 class Smartools(smartsheet.Smartsheet):
+
+    _impersonate = None
+    _callback = None
 
     def smartools(self):
         return True
+
+    def prepare_request(self, *args, **kwargs):
+        """
+        Prepare the request for the API call.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            dict: Prepared request.
+        """
+        request = super().prepare_request(*args, **kwargs)
+        if self._impersonate is not None:
+            try:
+                del request.headers["Assume-User"]
+            except KeyError:
+                pass
+            request.headers.update({"Smar-Impersonate": self._impersonate})
+        return request
+
+    def impersonate(self, user_string):
+        """Impersonate the selected user
+
+        Args:
+            user_string (str): String to impersonate.
+        """
+        self._impersonate = user_string
 
     def __getattr__(self, name):
         """
@@ -20,43 +52,47 @@ class Smartools(smartsheet.Smartsheet):
         Returns:
             Instance of named class.
         """
-        if name == 'smartools':
+        if name == "smartools":
             return self.smartools
         try:
             # smartools api class first
-            class_ = getattr(importlib.import_module(
-                __package__ + '.operations.' + name.lower()), 'Smartools' + name)
+            class_ = getattr(
+                importlib.import_module(__package__ + ".operations." + name.lower()),
+                "Smartools" + name,
+            )
             return class_(self)
         except ImportError:
             # smartools model class next
             try:
-                class_ = getattr(importlib.import_module(
-                    __package__ + '.models.' + name.lower(), 'Smartools' + name
-                ))
-                class_ = getattr(importlib.import_module(
-                    name.lower()), name)
+                class_ = getattr(
+                    importlib.import_module(
+                        __package__ + ".models." + name.lower(), "Smartools" + name
+                    )
+                )
+                class_ = getattr(importlib.import_module(name.lower()), name)
                 return class_()
             except ImportError:
-            # smartsheet classes next
+                # smartsheet classes next
                 return super().__getattr__(name)
+
 
 class SmartoolsOperationResult(smartsheet.smartsheet.OperationResult):
 
     def native(self, expected):
         try:
-            if expected != 'DownloadedFile':
+            if expected != "DownloadedFile":
                 data = self.resp.json()
             else:
                 filename = re.findall(
-                        'filename="(.+)";',
-                        self.resp.headers['Content-Disposition'])
+                    'filename="(.+)";', self.resp.headers["Content-Disposition"]
+                )
 
                 data = {
-                    'resultCode': 0,
-                    'message': 'SUCCESS',
-                    'resp': self.resp,
-                    'filename': filename[0],
-                    'downloadDirectory': self.operation['dl_path']
+                    "resultCode": 0,
+                    "message": "SUCCESS",
+                    "resp": self.resp,
+                    "filename": filename[0],
+                    "downloadDirectory": self.operation["dl_path"],
                 }
         except ValueError:
             return OperationErrorResult(self.op_result, self.resp)
@@ -65,29 +101,30 @@ class SmartoolsOperationResult(smartsheet.smartsheet.OperationResult):
             klass = expected[0]
             dynamic_type = expected[1]
             try:
-                class_ = getattr(importlib.import_module(
-                    'smartools.models'), 'Smartools' + klass)
+                class_ = getattr(
+                    importlib.import_module("smartools.models"), "Smartools" + klass
+                )
             except AttributeError:
-                class_ = getattr(importlib.import_module(
-                    'smartsheet.models'), klass)
+                class_ = getattr(importlib.import_module("smartsheet.models"), klass)
             obj = class_(data, dynamic_type, self._base)
-            if hasattr(obj, 'request_response'):
+            if hasattr(obj, "request_response"):
                 obj.request_response = self.resp
 
             return obj
 
         try:
-            class_ = getattr(importlib.import_module(
-                'smartools.models'), 'Smartools' + expected)
+            class_ = getattr(
+                importlib.import_module("smartools.models"), "Smartools" + expected
+            )
         except AttributeError:
-            class_ = getattr(importlib.import_module(
-                'smartsheet.models'), expected)
+            class_ = getattr(importlib.import_module("smartsheet.models"), expected)
 
         obj = class_(data, self._base)
-        if hasattr(obj, 'request_response'):
+        if hasattr(obj, "request_response"):
             obj.request_response = self.resp
 
         return obj
+
 
 # Perform Monkey Patches
 smartsheet.smartsheet.OperationResult = SmartoolsOperationResult
