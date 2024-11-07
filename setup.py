f597d73d371de1a9230acc94b2871f0a13cfb2f3
David Carli-Arnold <davocarli@gmail.com>
2024-11-07 14:04:04 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -2,30 +2,38 @@ import os
 
 from distutils.core import setup
 
+
 def read_file(filename):
     with open(os.path.join(os.path.dirname(__file__), filename)) as file:
         return file.read()
 
+
 setup(
-  name = 'smartools',
-  packages = ['smartools', 'smartools.models', 'smartools.models.enums', 'smartools.operations', 'smartools.types'],
-  version = '1.2.2',
-  license='MIT',
-  description = 'A wrapper for the smartsheet-python-sdk that monkey-patches in new methods & functionality.',
-  long_description=read_file('README.md'),
-  long_description_content_type='text/markdown',
-  author = 'David Carli-Arnold',
-  author_email = 'davocarli@gmail.com',
-  url = 'https://github.com/davocarli',
-  keywords = ['Smartsheet', 'smartsheet-python-sdk', 'monkey-patch'],
-  install_requires=[
-          'smartsheet-python-sdk',
-      ],
-  classifiers=[
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-  ],
+    name="smartools",
+    packages=[
+        "smartools",
+        "smartools.models",
+        "smartools.models.enums",
+        "smartools.operations",
+        "smartools.types",
+    ],
+    version="1.3.0",
+    license="MIT",
+    description="A wrapper for the smartsheet-python-sdk that monkey-patches in new methods & functionality.",
+    long_description=read_file("README.md"),
+    long_description_content_type="text/markdown",
+    author="David Carli-Arnold",
+    author_email="davocarli@gmail.com",
+    url="https://github.com/davocarli",
+    keywords=["Smartsheet", "smartsheet-python-sdk", "monkey-patch"],
+    install_requires=[
+        "smartsheet-python-sdk",
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
 )
